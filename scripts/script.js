e0cf271459a6736c938a74bfbfd81f9a302e3e2a
John Fourchy <fourchy151@gmail.com>
2023-07-14 22:52:36 -0700

    Single operation logic added

---
+++ scripts/script.js
@@ -0,0 +1,91 @@
+let firstNum = "";
+let secondNum = "";
+let op = "";
+const displayOb = document.querySelector(".display");
+const numbers = document.querySelectorAll(".btn");
+const operators = document.querySelectorAll(".op");
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    let res = b != 0 ? a / b : "Error";
+    return res;
+}
+
+function operate(num1, num2, op) {
+    let result = "";
+    if(op == "add") {
+        result = add(num1, num2);
+    } else if(op == "subtract") {
+        result = subtract(num1, num2);
+    } else if(op == "multiply") {
+        result = multiply(num1, num2);
+    } else if(op == "divide") {
+        result = divide(num1, num2);
+    }
+    return result;
+}
+
+function display(res) {
+    displayOb.textContent = res;
+}
+
+function setNumber(number) {
+    if(op === "") {
+        firstNum += number;
+        display(firstNum);
+    } else if(op !== "") {
+        secondNum += number;
+        display(secondNum);
+    }
+}
+
+function handleOperator(operator) {
+    if(operator === "add" || operator === "subtract" || operator === "multiply" || operator === "divide") {
+        op = operator;
+    } else if(operator === "clear") {
+        op = "";
+        firstNum = "";
+        secondNum = "";
+        display(0);
+    } else if(operator === "equal") {
+        if(firstNum !== "" && secondNum !== "" && op != "") {
+            const res = operate(firstNum, secondNum, op);
+            display(res);
+        }
+    }
+}
+
+function addNumberListeners() {
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+            setNumber(number.textContent);
+        });
+    });
+}
+
+function addOperatorListeners() {
+    operators.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            handleOperator(operator.className.split(" ")[1]);
+        });
+    });
+}
+
+// call these in a setup function
+addNumberListeners();
+addOperatorListeners();
+
+
+
+
